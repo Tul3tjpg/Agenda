82d488edb3c612c00661b0f00c5e2f8863324c05
Fernanda Juliette� Pasillas Martinez <fernanda_pasillas24@unikino.edu.mx>
2025-09-02 07:21:42 -0600

    Add project files.

---
+++ Agenda/Contacto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    internal class Contacto
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+    }
+}
